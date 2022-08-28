ee3cbe9d0903bf4f07d1a88142d99d0d1da24a90
CyrilBaah <cyril.baah@amalitech.org>
2022-08-28 22:00:30 +0000

    result method

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -16,8 +16,8 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You are looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', { 'question': question })
 
 
 def vote(request, question_id):
@@ -33,5 +33,4 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-    return HttpResponse("You are voting on question %s." % question_id)
 
