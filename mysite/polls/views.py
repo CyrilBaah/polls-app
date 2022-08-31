bec273953a5db0ca1d532eee7506b54c1fc1cbf3
CyrilBaah <cyril.baah@amalitech.org>
2022-08-31 01:18:22 +0000

    more test cases

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -18,8 +18,8 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
     
-    # def get_queryset(self):
-    #     return Question.objects.filter(pub_date__lte=timezone.now())
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
