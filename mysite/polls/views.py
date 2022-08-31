3a6e9ade70bbc9979c58d57a3931cec14640c9c2
CyrilBaah <cyril.baah@amalitech.org>
2022-08-31 01:34:38 +0000

    Comment views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -11,6 +11,10 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
         return Question.objects.filter(pub_date__lte=timezone.now())
 
 
@@ -19,6 +23,9 @@ class DetailView(generic.DetailView):
     template_name = 'polls/detail.html'
     
     def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
         return Question.objects.filter(pub_date__lte=timezone.now())
 
 
