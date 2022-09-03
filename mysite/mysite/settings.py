258167a64025ff66af6b999c5315b3b2ed2806fc
CyrilBaah <cyril.baah@amalitech.org>
2022-09-03 00:53:34 +0000

    remove print out comments

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -142,14 +142,12 @@ USE_TZ = True
 
 # STATIC_URL = 'static/'
 if os.getenv('USE_S3_BUCKET') == 'False':
-    print("Use local storage")
     STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
     MEDIA_URL = '/media/'
     
 else:
-    print("Use S3 Bucket")
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
@@ -163,9 +161,6 @@ else:
     PUBLIC_MEDIA_LOCATION = 'media'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
     DEFAULT_FILE_STORAGE = "_config.storages_backend.PublicMediaStorage"
-
-
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
