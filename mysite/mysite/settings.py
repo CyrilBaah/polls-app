eaff17759f4a37b315edb45a1c9d21b9ff8a5d58
CalebAckom <calebackom@amalitech.com>
2022-11-18 10:42:01 +0000

    Scraping with Prometheus

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'polls',
     'storages',
+    'django_prometheus',
 ]
 
 MIDDLEWARE = [
@@ -55,6 +56,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_prometheus.middleware.PrometheusBeforeMiddleware',
+    'django_prometheus.middleware.PrometheusAfterMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
